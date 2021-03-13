535ac8028b3bbf4dd081603594c4436cfe5ec9c5
One Time Productions Time <jaysentsao@outlook.com>
2021-03-13 00:10:00 +0000

    lol! aagames!

--- js-cord/index.js
+++ js-cord/index.js
@@ -10,16 +10,16 @@ module.exports = {
     Cooldown: require("./commands/Cooldown"),
 
     AllowedMentions: require("./structures/AllowedMentions"),
+    Channel: require("./structures/Channel"),
     ClientUser: require("./structures/ClientUser"),
     Embed: require("./structures/Embed"),
     Guild: require("./structures/Guild"),
     Member: require("./structures/Member"),
     Message: require("./structures/Message"),
-    Messageable: require("./structures/Messageable"),
+    Permissions: require("./structures/Permissions"),
+    Reaction: require("./structures/Reaction"),
     Role: require("./structures/Role"),
-    TextChannel: require("./structures/TextChannel"),
     User: require("./structures/User"),
-    VoiceChannel: require("./structures/VoiceChannel"),
 
     errors: {
         ConnectionError: ConnectionError,
