e4b48955a699313611c3587fd4fad188bf3316ea
One Time Productions Time <jaysentsao@outlook.com>
2021-03-13 04:29:02 +0000

    aaagames. i changed aagamex

--- js-cord/http/Requester.js
+++ js-cord/http/Requester.js
@@ -13,7 +13,7 @@ class Requester {
         this.userAgent = 'DiscordBot (js-cord 1.0)';
     }
 
-    request(route, body=null, contentType="application/json") {
+    request(route, requestBody=null, contentType="application/json") {
         let method = route.method.toLowerCase();
         let headers = {
             'Content-Type': contentType,
@@ -21,9 +21,9 @@ class Requester {
             'X-Ratelimit-Precision': 'millisecond',
             'Authorization': this.botToken ? `Bot ${this.token}` : this.token
         }
-        if (body) body['Content-Type'] = contentType;
+        if (requestBody) requestBody['Content-Type'] = contentType;
 
-        const response = needle.request(method, route.url, (body || {}), {json: true, headers: headers}, (
+        const response = needle.request(method, route.url, (reqeuestBody || {}), {json: true, headers: headers}, (
             err, { statusCode, body }
         ) => {
             if (err) throw err;
