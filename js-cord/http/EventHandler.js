6b9b586d6652cfd604af8d77e0a9aad79d4823b0
One Time Productions Time <jaysentsao@outlook.com>
2021-03-14 05:51:03 +0000

    ITS ABOUT TO WORK

--- js-cord/http/EventHandler.js
+++ js-cord/http/EventHandler.js
@@ -4,12 +4,12 @@ const Message = require("../structures/Message");
 module.exports = function handleEvent(client, event, data) {
     event = event.toUpperCase().replace(" ", "_");
     if (event === "READY") {
-        this.client.emit("ready");
-        this.client.user = new ClientUser(client, data['user']);
+        client.emit("ready");
+        client.user = new ClientUser(client, data['user']);
     } else if (event === "RESUMED") {
-        this.client.emit("resumed");
+        client.emit("resumed");
     } else if (event === "RECONNECT") {
-        this.client.emit("reconnect") 
+        client.emit("reconnect") 
     } 
 
     // implement on_ready_whitelist
@@ -25,20 +25,20 @@ module.exports = function handleEvent(client, event, data) {
 
     // channels
     else if (event === "CHANNEL_CREATE") {
-        this.client.emit("channel")
+        client.emit("channel")
     } // wip because channel support is currently partial
 
     // messages
     else if (event === "MESSAGE_CREATE") {
-        this.client.emit("message", Message.fromData(data));
+        client.emit("message", Message.fromData(data));
         // somehow, we add this message to the cache
     } // we need to make a message cache, or else 
     // MESSAGE_EDIT/MESSAGE_DELETE won't be able to take payloads
     else if (event === "MESSAGE_UPDATE") {
-        this.client.emit("messageEdit", null /*some way to get the old message*/, Message.fromData(data));
+        client.emit("messageEdit", null /*some way to get the old message*/, Message.fromData(data));
     } else if (event === "MESSAGE_DELETE") {
-        this.client.emit("messageDelete", null /*again, some way to get the old message*/)
+        client.emit("messageDelete", null /*again, some way to get the old message*/)
     } else if (event === "MESSAGE_DELETE_BULK") {
-        this.client.emit("messageBulkDelete", null /*what i said above, but iterate through id's*/)
+        client.emit("messageBulkDelete", null /*what i said above, but iterate through id's*/)
     }
 }
