f15d5a1b3bad5c042c555ddc3eb27bbc1b196d5f
One Time Productions Time <jaysentsao@outlook.com>
2021-03-15 02:18:36 +0000

    FOAM

--- js-cord/structures/Intents.js
+++ js-cord/structures/Intents.js
@@ -5,7 +5,7 @@ const bitLengthOf = n => n.toString(2).match(/1/g).length;
 
 module.exports = class Intents extends Flags {
     constructor(options) {
-        super(constants.INTENT_FLAG_VALUES);
+        super(Constants.INTENT_FLAG_VALUES);
         for (option of Object.keys(Constants.INTENT_FLAG_VALUES)) {
             if (Object.keys(options).includes(option)) {
                 Object.defineProperty(this, option, options[option]);
