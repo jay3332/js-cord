1371c76c04bd3e98b954341f83940b1cc5dcb459
Programmer03 <squirtlepokemongo03@gmail.com>
2021-03-15 15:37:28 +0000

    snowflakes -_-

--- js-cord/util/Util.js
+++ js-cord/util/Util.js
@@ -14,21 +14,25 @@ class Util {
     static parseSnowflake(snowflake) {
         const epoch = 1420070400000;
         let binary = '';
-        if (!['number', 'string', 'date'].includes(typeof snowflake) || !snowflake) return;
-        snowflake = snowflake.toString()
-        let high = parseInt(snowflake.slice(0, -10));
-        let low = parseInt(snowflake.slice(-10));
-        while (high > 0 || low > 0) {
-            binary = String(low&1) + binary;
-            low = Math.floor(low / 2);
-            if (high > 0) {
-                low += 5000000000 * (high % 2);
-                high = Math.floor(high / 2);
+        try {
+            snowflake = snowflake.toString()
+            let high = parseInt(snowflake.slice(0, -10));
+            let low = parseInt(snowflake.slice(-10));
+            while (high > 0 || low > 0) {
+                binary = String(low&1) + binary;
+                low = Math.floor(low / 2);
+                if (high > 0) {
+                    low += 5000000000 * (high % 2);
+                    high = Math.floor(high / 2);
+                }
             }
+            binary = binary.toString(2).padStart(64, '0');
+            const unix = parseInt(binary.substring(0, 42), 2)+epoch;
+            return new Date(unix);
         }
-        binary = binary.toString(2).padStart(64, '0');
-        const unix = parseInt(binary.substring(0, 42), 2)+epoch;
-        return new Date(unix);
+        catch {
+            return false;
+        }
     }
 
 	static escapeMarkdown(text, {
