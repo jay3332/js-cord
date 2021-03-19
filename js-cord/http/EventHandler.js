a79567ad3c035f57a345c568517b10d4568c9665
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-03-19 19:38:10 -0400

    test

--- js-cord/http/EventHandler.js
+++ js-cord/http/EventHandler.js
@@ -8,7 +8,7 @@ const Message = require("../structures/Message");
 const Guild = require("../structures/Guild");
 
 const whitelist = [
-    "READY", "RESUMED", 
+    "READY", "RESUMED",
     "GUILD_CREATE", "GUILD_DELETE",
     "GUILD_MEMBER_ADD", "GUILD_MEMBER_REMOVE",
     "GUILD_MEMBERS_CHUNK"
@@ -26,17 +26,17 @@ module.exports = async function handleEvent(client, event, data) {
     } else if (event === "RESUMED") {
         await client.emit("resumed");
     } else if (event === "RECONNECT") {
-        await client.emit("reconnect") 
-    } 
+        await client.emit("reconnect")
+    }
 
-    // guilds 
+    // guilds
     else if (event === "GUILD_CREATE") {
         const unavailable = data.unavailable || null;
         if (unavailable) return;
 
         const guild = new Guild(client, data);
         client.cache.addGuild(guild);
-        if (unavailable === false) 
+        if (unavailable === false)
             await client.emit("guildAvailable", [ guild ]);
         else
             await client.emit("guildJoin", [ guild ]);
@@ -52,14 +52,14 @@ module.exports = async function handleEvent(client, event, data) {
         let msg = new Message(client, data);
         client.cache.addMessage(msg);
         await client.emit("message", [ msg ]);
-    } 
+    }
     else if (event === "MESSAGE_UPDATE") {
         let rawEvent = data;
         let newMsg = new Message(client, data);
         client.cache.addMessage(newMsg);
         let oldMsg = client.cache.getMessage(data.channel_id, data.id);
         if (oldMsg) rawEvent.cachedMessage = oldMsg;
-        await client.emit("rawMessageEdit", [ rawEvent ]);
+        await client.emit("rawMessageEdit", [ rawEvent ]); 
         await client.emit("messageEdit", [oldMsg, newMsg]);
     } else if (event === "MESSAGE_DELETE") {
         await client.emit("rawMessageDelete", [ data ]);
