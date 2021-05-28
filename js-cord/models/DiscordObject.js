d1c15c81ef281818da4a1a6a01ce87eab719f305
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-05-28 16:11:37 -0400

    Document DiscordObject

--- js-cord/models/DiscordObject.js
+++ js-cord/models/DiscordObject.js
@@ -1,8 +1,20 @@
 const { parseSnowflake } = require('../utils');
 
+/**
+ * Represents a Discord object with a snowflake.
+ */
 module.exports = class DiscordObject {
     constructor(id) {
+        /**
+         * @type {Date}
+         * The date and time that this object was created.
+         */
         this.createdAt = parseSnowflake(id);
+
+        /**
+         * @type {string}
+         * The ID of this object.
+         */
         this.id = id;
     }
 }
