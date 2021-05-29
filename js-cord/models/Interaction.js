9b548dec3f38e8921b9bc2c92959b50533a84504
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-05-29 18:10:29 -0400

    Document interaction

--- js-cord/models/Interaction.js
+++ js-cord/models/Interaction.js
@@ -14,18 +14,46 @@ module.exports = class Interaction extends DiscordObject {
     }
 
     loadData(data) {
+        /**
+         * The interaction token to send to Discord.
+         * @type {string}
+         */
         this.token = data.token;
+
+        /**
+         * This is always 1.
+         * @type {integer}
+         */
         this.version = data.version;  // Always 1
+
+        /**
+         * The ID of the application (user) that received the interaction.
+         * This is usually always your bot's ID.
+         * @type {string} 
+         */
         this.applicationID = data.application_id;
+
+        /**
+         * The type of interaction this is.
+         * @see enums.InteractionType
+         * @type {integer}
+         */
         this.type = data.type;
+        
         this._data = data.data;
 
         if (this._data) {      
             let buffer;
+
+            /**
+             * The interaction data, such as the options provided in the interaction.
+             * @type {?object}
+             */
             this.data = {
                 id: this._data.id,
                 name: this._data.name
             }
+
             if (buffer = this._data.custom_id)
                 this.data.customID = buffer;
             if (buffer = this._data.component_type)
@@ -77,19 +105,37 @@ module.exports = class Interaction extends DiscordObject {
         }
 
         if (data.channel_id)
+            /**
+             * The channel this interaction was sent in.
+             * @type {?TextChannel | DMChannel}
+             */
             this.channel = this.client.getChannel(data.channel_id);
         if (data.guild_id)
+            /**
+             * The guild this interaction was sent in.
+             * @type {?Guild}
+             */
             this.guild = this.client.getGuild(data.guild_id);
 
         if (data.message) {
             const obj = new Message(this.client, data.message);
             this.client.cache.messages.push(obj);
+
+            /**
+             * The interaction message.
+             * @type {Message}
+             */
             this.message = obj;
         }
 
         if (data.member) {
             const obj = new Member(this.client, this.guild, data.member);
             this.client.cache.users.push(obj.toUser());
+
+            /**
+             * The user that made this interaction.
+             * @type {User | Member}
+             */
             this.author = obj;
         } else if (data.user) {
             const obj = new User(this.client, data.user);
@@ -106,6 +152,12 @@ module.exports = class Interaction extends DiscordObject {
         return await this.respond(content, options, { edit: true });
     }
 
+    /**
+     * Responds to the interaction.
+     * @param {?string} content The content of the message.
+     * @param {?options} options The options to pass into the {@link MessageBuilder}. 
+     * @returns {?Message} The message sent, if there was one.
+     */
     async respond(content, options, __overwrites = {}) {
         const builder = new MessageBuilder(
             this.channel, 
